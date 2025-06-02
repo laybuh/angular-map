f679c852c1e6b7c360514c013645c3542bd4063d
nkha129 <nkha129@wgu.edu>
2025-06-02 17:00:13 -0500

    Added SVG Map.

---
+++ map-app/src/app/map-comp/map-comp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-comp',
+  imports: [],
+  templateUrl: './map-comp.component.html',
+  styleUrl: './map-comp.component.css'
+})
+export class MapCompComponent {
+
+}
